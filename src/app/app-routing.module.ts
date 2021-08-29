178279da280edd76ce91dea2c79a47849302d977
Ramanan <rramananraja@gmail.com>
2021-08-29 11:19:05 +0530

    initial push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './admin/users/users.component';
+import { BooksComponent } from './admin/books/books.component';
+import { ShopbookComponent } from './users/shopbook/shopbook.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin/users', component: UsersComponent },
+  { path: 'admin/books', component: BooksComponent },
+  { path: 'user/shop', component: ShopbookComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
